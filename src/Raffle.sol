205b94be435f4805c358f5459a019735ad3f48be
aliweb3 <114404873+aliweb3@users.noreply.github.com>
2023-06-27 23:34:23 +0330

    0.0.3

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,21 +2,32 @@
 
 pragma solidity ^0.8.18;
 
+
+
+
 /***
  * @title A lottery Contract
  * @author Ali Haghshenas
  * @dev Implement Chainlink VRFv2
  */
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+
 contract Raffle {
     /***Errors */
     error Raffle__NotEnoughEthSent();
+
+    uint256 private constant REQUEST_CONFIRMATIONS = 3;
+    uint256 private constant NUM_WORDS = 1;
 
     uint256 private immutable i_entranceFee;
     // @dev duration of the lottery is seconds
     uint256 private immutable i_interval;
     address private immutable i_vrfCoordinator;
     bytes32 private immutable i_gasLane;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
 
     address payable[] private s_players;
 
@@ -26,12 +37,14 @@ contract Raffle {
     event EnteredRaffle(address indexed player);
 
 
-    constructor(uint256 entranceFee, uint256 interval , address vrfCoordinator , bytes32 gasLane) {
+    constructor(uint256 entranceFee, uint256 interval , address vrfCoordinator , bytes32 gasLane , uint256 subscriptionId , uint32 callbackGasLimit) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
         i_vrfCoordinator = vrfCoordinator;
         i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit ;
     }
 
     function enterRaffle() external payable {
@@ -55,10 +68,10 @@ contract Raffle {
         // Will revert if subscription is not set and funded.
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane, // gas lane 
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS 
         );
 
     /**Getter Function */
