8efbdd8e4d7d0fc4f47e44872ab2972dba7e848e
aliweb3 <114404873+aliweb3@users.noreply.github.com>
2023-06-28 18:06:13 +0330

    0.0.6

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -16,6 +16,7 @@ contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughEthSent();
     error Raffle__TransferFailed();
     error Raffle__RaffleNotOpen();
+    error Raffle__UpKeepNotNeeded(uint256 currentBalance , uint256 numPlayers , uint256 raffleState);
 
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
@@ -76,12 +77,40 @@ contract Raffle is VRFConsumerBaseV2 {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() external {
-        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
-            revert();
+
+
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) public view returns (bool upkeepNeeded, bytes memory /*performData*/) {
+
+         bool timeHasPassed =(block.timestamp - s_lastTimeStamp) >= i_interval ;
+         bool isOpen = RaffleState.OPEN == s_raffleState;
+         bool hasBalance = address(this).balance > 0;
+         bool hasPlayers = s_players.length>0;
+         upkeepNeeded = (timeHasPassed && isOpen && hasBalance && hasPlayers);
+         return (upkeepNeeded , "0x0");
+
+       
+
+    }
+
+
+    // pickWinner
+
+    function performUpkeep(bytes calldata /* performData */) external {
+
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded){
+            revert Raffle__UpKeepNotNeeded(
+                address(this).balance ,
+                s_players.length,
+                uint256(s_raffleState)
+            );
         }
+
+
         s_raffleState = RaffleState.CALCULATING;
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+      i_vrfCoordinator.requestRandomWords(
             i_gasLane, // gas lane
             i_subscriptionId,
             REQUEST_CONFIRMATIONS,
@@ -91,7 +120,7 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function fulfillRandomWords(
-        uint256 requestId,
+        uint256  /*requestId */,
         uint256[] memory randomWords
     ) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
