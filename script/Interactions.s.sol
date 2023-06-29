f12688ff5eacc1125fbcedf73fc4ead1d2c94a5d
aliweb3 <114404873+aliweb3@users.noreply.github.com>
2023-06-29 15:16:28 +0330

    FundSubscription contract add

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -14,19 +14,85 @@ contract CreateSubscriptions is Script {
         return  createSubscription(vrfCoordinator);
     }
 
-    function createSubscription(address vrfCoordinator) public returns(uint65) [
-        console.log("Creating subscription on chainId:" ,block.chainid);
-        vm.startBroadcast();
-    uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator).createSubscription();
+
+//////////////////////////
+    function createSubscription(
+        address vrfCoordinatorV2,
+        uint256 deployerKey
+    ) public returns (uint64) {
+        console.log("Creating subscription on chainId: ", block.chainid);
+        vm.startBroadcast(deployerKey);
+        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinatorV2)
+            .createSubscription();
         vm.stopBroadcast();
-        
-        console.log("Your sun Id is :", subId);
-        return subid;
-    ]
+        console.log("Your subscription Id is: ", subId);
+        console.log("Please update the subscriptionId in HelperConfig.s.sol");
+        return subId;
+    }
+
+
+
+/////////////
 
 
 
     function run() external returns (uint256) {
         return createSubscriptionUsingConfig();
+    }
+}
+
+
+
+contract FundSubscription is Script {
+    uint96 public constant FUND_AMOUNT = 3 ether;
+
+    function fundSubscriptionUsingConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint64 subId,
+            ,
+            ,
+            ,
+            ,
+            address vrfCoordinatorV2,
+            address link,
+            uint256 deployerKey
+        ) = helperConfig.activeNetworkConfig();
+        fundSubscription(vrfCoordinatorV2, subId, link, deployerKey);
+    }
+
+    function fundSubscription(
+        address vrfCoordinatorV2,
+        uint64 subId,
+        address link,
+        uint256 deployerKey
+    ) public {
+        console.log("Funding subscription: ", subId);
+        console.log("Using vrfCoordinator: ", vrfCoordinatorV2);
+        console.log("On ChainID: ", block.chainid);
+        if (block.chainid == 31337) {
+            vm.startBroadcast(deployerKey);
+            VRFCoordinatorV2Mock(vrfCoordinatorV2).fundSubscription(
+                subId,
+                FUND_AMOUNT
+            );
+            vm.stopBroadcast();
+        } else {
+            console.log(LinkToken(link).balanceOf(msg.sender));
+            console.log(msg.sender);
+            console.log(LinkToken(link).balanceOf(address(this)));
+            console.log(address(this));
+            vm.startBroadcast(deployerKey);
+            LinkToken(link).transferAndCall(
+                vrfCoordinatorV2,
+                FUND_AMOUNT,
+                abi.encode(subId)
+            );
+            vm.stopBroadcast();
+        }
+    }
+
+    function run() external {
+        fundSubscriptionUsingConfig();
     }
 }
